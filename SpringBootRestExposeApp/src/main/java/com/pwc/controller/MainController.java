dee8018c636e5f82de13c5221d7ebb5e500177d5
sibsankarb814 <sibsankarb814@IN5CD8052Q4R.IN.nam.ad.pwcinternal.com>
2018-06-06 13:12:00 +0530

    rest api expose api

---
+++ SpringBootRestExposeApp/src/main/java/com/pwc/controller/MainController.java
@@ -0,0 +1,112 @@
+package com.pwc.controller;
+
+import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pwc.dao.EmployeeDAO;
+import com.pwc.model.Employee;
+
+@RestController
+
+public class MainController {
+
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+ 
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+    	System.out.println("In Controller ");
+	        return "Welcome to PwC Employee Management System.";
+    }
+
+    
+    // URL:
+    // http://localhost:8080/SomeContextPath/employees
+    // http://localhost:8080/SomeContextPath/employees.xml
+    // http://localhost:8080/SomeContextPath/employees.json
+    @RequestMapping(value = "/employees", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    		  
+    @ResponseBody
+    public List<Employee> getEmployees() {
+        List<Employee> list = employeeDAO.getAllEmployees();
+        return list;
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee/{empNo}
+    // http://localhost:8080/SomeContextPath/employee/{empNo}.xml
+    // http://localhost:8080/SomeContextPath/employee/{empNo}.json
+    @RequestMapping(value = "/employee/{empNo}", 
+            method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+            //produces = { MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee getEmployee(@PathVariable("empNo") String empNo) {
+        return employeeDAO.getEmployee(empNo);
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee
+    // http://localhost:8080/SomeContextPath/employee.xml
+    // http://localhost:8080/SomeContextPath/employee.json
+ 
+    @RequestMapping(value = "/employee", 
+            method = RequestMethod.POST, 
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Employee addEmployee(@RequestBody Employee emp) {
+ 
+        System.out.println("(Service Side) Creating employee: " + emp.getEmpNo());
+ 
+        return employeeDAO.addEmployee(emp);
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee
+    // http://localhost:8080/SomeContextPath/employee.xml
+    // http://localhost:8080/SomeContextPath/employee.json
+    @RequestMapping(value = "/employee", 
+            method = RequestMethod.PUT, 
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Employee updateEmployee(@RequestBody Employee emp) {
+ 
+        System.out.println("(Service Side) Editing employee: " + emp.getEmpNo());
+ 
+        return employeeDAO.updateEmployee(emp);
+    }
+ 
+    // URL:
+    // http://localhost:8080/SomeContextPath/employee/{empNo}
+    @RequestMapping(value = "/employee/{empNo}", 
+            method = RequestMethod.DELETE, 
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public void deleteEmployee(@PathVariable("empNo") String empNo) {
+ 
+        System.out.println("(Service Side) Deleting employee: " + empNo);
+ 
+        employeeDAO.deleteEmployee(empNo);
+    }
+ 
+	
+}
